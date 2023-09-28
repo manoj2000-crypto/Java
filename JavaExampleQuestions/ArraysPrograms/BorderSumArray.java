1bb9426d33800d42c829d14769b1adc31067fbe7
Manoj Kale <manojkale2000@gmail.com>
2023-09-28 08:50:14 +0530

    Making same program dynamic by getting values from the user.

--- JavaExampleQuestions/ArraysPrograms/BorderSumArray.java
+++ JavaExampleQuestions/ArraysPrograms/BorderSumArray.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
 
 public class BorderSumArray {
 	
 	public void borderSum() 
 	{
-		int[][] arr = {{1,2,3,4},{2,3,4,5},{5,6,7,8},{1,1,1,1}};
+		Scanner sc = new Scanner(System.in);
+		System.out.print("How many row do you want : ");
+		int row = sc.nextInt();
+		System.out.print("How many column do you want : ");
+		int col = sc.nextInt();
+		
+		//int[][] arr = {{1,2,3,4},{2,3,4,5},{5,6,7,8},{1,1,1,1}};
+		
+		int[][] arr = new int[row][col];
 		int sum = 0;
+		
+		System.out.print("Enter the elements [ " + row + " ] by [ " + col + " ] :");
+		for(int i = 0; i<arr.length; i++)
+		{
+			for(int j = 0; j<arr.length ; j++)
+			{	
+				arr[i][j] = sc.nextInt();
+			}
+		}
+		System.out.println("\nElements are given below : ");
 		
 		for(int i = 0; i<arr.length; i++)
 		{
@@ -15,7 +34,7 @@ public class BorderSumArray {
 			System.out.println();
 		}
 		
-		System.out.println();
+		System.out.println("\nPrinting row as column : ");
 		
 		for(int i = 0; i<arr.length; i++)
 		{
@@ -31,7 +50,8 @@ public class BorderSumArray {
 			System.out.println();
 		}
 		
-		System.out.println("Sum is : " + sum);
+		System.out.println("\nSum is : " + sum);
+		sc.close();
 	}
 
 	public static void main(String[] args) {
