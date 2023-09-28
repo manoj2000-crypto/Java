89f5b90f383e82f7e46676856ee07102835a9858
Manoj Kale <manojkale2000@gmail.com>
2023-09-28 08:11:53 +0530

    Modified and code is optimized. Only using two for loops to calculate the sum of border array elements.

--- JavaExampleQuestions/ArraysPrograms/BorderSumArray.java
+++ JavaExampleQuestions/ArraysPrograms/BorderSumArray.java
@@ -4,8 +4,6 @@ public class BorderSumArray {
 	public static void main(String[] args) {
 		
 		int[][] arr = {{1,2,3,4},{2,3,4,5},{5,6,7,8},{1,1,1,1}};
-		int[][] arr2 = new int[4][4];
-		int[][] arr3 = new int[4][4];
 		int sum = 0;
 		
 		for(int i = 0; i<arr.length; i++)
@@ -23,35 +21,9 @@ public class BorderSumArray {
 			{	
 				if(i == 0 || i == 3) 
 				{
-					arr2[i][j] = arr[i][j];
+					sum = sum + arr[i][j];
+					sum = sum + arr[j][i];
 				}
-			}
-		}
-		
-		for(int m = 0; m<arr.length; m++)
-		{
-			for(int n = 0; n<arr.length ; n++)
-			{	
-				if(m == 0 || m== 3) 
-				{
-					arr3[m][n] = arr[m][n];
-				}
-			}
-		}
-		
-		for(int p = 0; p<arr.length; p++)
-		{
-			for(int q = 0; q<arr.length ; q++)
-			{	
-				sum = sum + arr2[p][q];
-			}
-		}
-		
-		for(int p = 0; p<arr.length; p++)
-		{
-			for(int q = 0; q<arr.length ; q++)
-			{	
-				sum = sum + arr3[p][q];
 			}
 		}
 		
